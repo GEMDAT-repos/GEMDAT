40e744941cc706213968ee615a0e032ddd099c4a
Victor Azizi <victor@lipsum.eu>
2023-06-15 09:21:05 +0200

    first few plots working and updated README.md with usage (#7)

--- src/plot_from_displacement.py
+++ src/plot_from_displacement.py
@@ -5,20 +5,20 @@ import numpy as np
 def plots_from_displacement(sim_data, sites, resolution):
     # Plot displacement for each diffusing atom:
     fig, ax = plt.subplots()
-    for i in range(sim_data.nr_atoms):
-        if sim_data.diffusing_atom[i]:
-            ax.plot(sim_data.displacement[i, :])
+    for i in range(int(sim_data['nr_atoms'])):
+        if sim_data['diffusing_atom'][0, 0][i]:
+            ax.plot(sim_data['displacement'][0, 0][i, :])
     ax.set(title='Displacement of diffusing element',
            xlabel='Time step',
            ylabel='Displacement (Angstrom)')
 
     # Plot histogram of final displacement of diffusing atoms:
-    displacement = np.zeros(sim_data.nr_diffusing)
+    displacement = np.zeros(int(sim_data['nr_diffusing']))
     count = 0
-    for i in range(sim_data.nr_atoms):
-        if sim_data.diffusing_atom[i]:
+    for i in range(int(sim_data['nr_atoms'])):
+        if sim_data['diffusing_atom'][0, 0][i]:
             count += 1
-            displacement[count - 1] = sim_data.displacement[i, -1]
+            displacement[count - 1] = sim_data['displacement'][0, 0][i, -1]
     fig, ax = plt.subplots()
     ax.hist(displacement)
     ax.set(title='Histogram of displacement of diffusing element',
@@ -27,14 +27,18 @@ def plots_from_displacement(sim_data, sites, resolution):
 
     # Plot displacement per element:
     counter = 0
-    disp_elem = np.zeros((sim_data.nr_elements, sim_data.nr_steps))
+    disp_elem = np.zeros(
+        (int(sim_data['nr_elements']), int(sim_data['nr_steps'])))
     fig, ax = plt.subplots()
-    for i in range(sim_data.nr_elements):
-        for j in range(counter, counter + sim_data.nr_per_element[i]):
-            disp_elem[i, :] += sim_data.displacement[j, :]
-        counter += sim_data.nr_per_element[i]
-        disp_elem[i, :] /= sim_data.nr_per_element[i]
+    for i in range(int(sim_data['nr_elements'])):
+        for j in range(
+                counter,
+                counter + int(sim_data['nr_per_element'][0, 0][i].item())):
+            disp_elem[i, :] += sim_data['displacement'][0, 0][j, :]
+        counter += int(sim_data['nr_per_element'][0, 0][i].item())
+        disp_elem[i, :] /= sim_data['nr_per_element'][0, 0][i]
         ax.plot(disp_elem[i, :])
     ax.set(title='Displacement per element',
            xlabel='Time step',
            ylabel='Displacement (Angstrom)')
+    plt.show()
