f63468619f617630ec81eb728ed6fff4b518497a
Stef Smeets <s.smeets@esciencecenter.nl>
2023-06-21 10:34:32 +0200

    Add imports

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,13 @@
+from .displacements import (
+    calculate_displacements,
+    plot_displacement_histogram,
+    plot_displacement_per_element,
+    plot_displacement_per_site,
+)
+
 __all__ = [
-    'get_displacements',
-    'plot_displacement_per_site'
+    'calculate_displacements',
+    'plot_displacement_per_site',
     'plot_displacement_per_element',
+    'plot_displacement_histogram',
 ]
