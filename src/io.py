ea6abad57d9f6c132cf57b26774d39d1fdcd820f
Stef Smeets <s.smeets@esciencecenter.nl>
2023-06-27 15:25:04 +0200

    Add io submodule to load cif data / known materials

--- src/io.py
+++ src/io.py
@@ -1,8 +1,13 @@
+from importlib.resources import files
+from pathlib import Path
+
 from pymatgen.core import Structure
 from pymatgen.io import cif
 
+DATA = Path(files('gemdat') / 'data')  # type: ignore
 
-def load_cif_as_structure(filename: str) -> Structure:
+
+def load_cif(filename: Path | str) -> Structure:
     """Load cif file and return first item as pymatgen structure."""
     cifdata = cif.CifParser(filename)
     structure = cifdata.get_structures(primitive=False)[0]
@@ -11,5 +16,9 @@ def load_cif_as_structure(filename: str) -> Structure:
 
 def load_known_material(name: str) -> Structure:
     """Load known material."""
-    filename = name
-    return load_cif_as_structure(filename)
+    filename = (DATA / name).with_suffix('.cif')
+
+    if not filename.exists():
+        raise ValueError(f'Unknown material: {name}')
+
+    return load_cif(filename)
--- tests/io_test.py
+++ tests/io_test.py
@@ -1,2 +1,8 @@
-def test_read_vasp():
-    assert (True)
+from gemdat.io import load_known_material
+from pymatgen.core import Structure
+
+
+def test_load_known_material():
+    structure = load_known_material('argyrodite')
+
+    assert isinstance(structure, Structure)
