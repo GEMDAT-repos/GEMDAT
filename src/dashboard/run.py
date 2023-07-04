b7e0451c7c9edb0a9decbc8db11a9b4a4bc256c1
Victor Azizi <victor@lipsum.eu>
2023-07-04 14:10:07 +0200

    Moved dashboard inside gemdat

--- src/dashboard/run.py
+++ src/dashboard/run.py
@@ -3,12 +3,9 @@ from pathlib import Path
 import streamlit as st
 from gemdat import SimulationData, plot_all
 
-#import pygwalker as pyg
-#import streamlit.components.v1 as components
-
 st.set_page_config(page_title='Gemdat gemdash dashboard', layout='wide')
 
-fig_tab, pyg_tab = st.tabs(['Figures', 'PyGWalker'])
+fig_tab, _ = st.tabs(['Figures', 'Other Tabs'])
 
 with st.sidebar:
     data_location = st.text_input('Location of data', 'vasprun.xml')
@@ -53,21 +50,3 @@ with fig_tab:
     for num, col in enumerate(st.columns(number_of_cols)):
         for figure in figures[num::number_of_cols]:
             col.pyplot(figure)
-
-with pyg_tab:
-    pass
-#    extra = data.calculate_all(equilibration_steps=1250, diffusing_element=diffusing_element)
-#
-#    # Add Title
-#    st.title('Use Pygwalker for GEMDAT')
-#
-#    # Import your data
-#    data_xr = xr.DataArray(extra['speed'],
-#                           dims=['element', 'time']).isel(time=slice(0, 1000))
-#    data_df = data_xr.to_dataframe(name='speed')
-#
-#    # Generate the HTML using Pygwalker
-#    pyg_html = pyg.walk(data_df, return_html=True)
-#
-#    # Embed the HTML into the Streamlit app
-#    components.html(pyg_html, height=1000, scrolling=True)
