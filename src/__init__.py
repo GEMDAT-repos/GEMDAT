3685fc8b5fdff5d204e08d21dce71988381ef1b4
Stef Smeets <s.smeets@esciencecenter.nl>
2023-07-11 10:25:12 +0200

    Expose functions to api

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,13 @@
 from .data import SimulationData
+from .io import load_cif, load_known_material
 from .plot import plot, plot_all
+from .sites import SitesData
 
 __all__ = [
+    'load_cif',
+    'load_known_material',
     'plot',
     'plot_all',
     'SimulationData',
+    'SitesData',
 ]
