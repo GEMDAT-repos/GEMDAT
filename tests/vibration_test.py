6a6c943b6771e20d35e313b5766100d45dace972
Victor Azizi <victor@lipsum.eu>
2023-07-14 13:52:26 +0200

    added vibration test

--- tests/vibration_test.py
+++ tests/vibration_test.py
@@ -1,5 +1,34 @@
+from types import SimpleNamespace
+
 import numpy as np
+from gemdat import SimulationData
+from gemdat.calculate import Vibration
 from gemdat.calculate.vibration import meanfreq
+
+data = SimulationData(
+    time_step=1,
+    trajectory_coords=None,
+    lattice=None,
+    species=None,
+    temperature=None,
+    parameters=None,
+    structure=None,
+)
+extras = SimpleNamespace(diff_displacements=np.array(
+    [[0., 0.73654599, 0.10440307, 0.70356236, 0.17204651]]), )
+
+
+def test_vibration_calculate_all():
+    ret = Vibration.calculate_all(data, extras)
+    assert (np.allclose(
+        ret['speed'],
+        np.array([[0., 0.73654599, -0.63214292, 0.59915929, -0.53151585]])))
+    assert (np.isclose(ret['attempt_freq'], 0.38779581521997086))
+    assert (np.isclose(ret['attempt_freq_std'], 0.))
+    assert (np.allclose(
+        ret['amplitudes'],
+        np.array([0.73654599, -0.63214292, 0.59915929, -0.53151585])))
+    assert (np.isclose(ret['vibration_amplitude'], 0.6277351391878859))
 
 
 def test_meanfreq_single_timestep():
