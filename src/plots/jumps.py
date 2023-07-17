a598137a596a4b47b06b4aeaf7a82316e11a4f97
Stef Smeets <s.smeets@esciencecenter.nl>
2023-07-17 15:28:57 +0200

    Add 3d jumps plot

--- src/plots/jumps.py
+++ src/plots/jumps.py
@@ -4,6 +4,7 @@ from typing import TYPE_CHECKING
 
 import matplotlib.pyplot as plt
 import numpy as np
+from pymatgen.electronic_structure import plotter
 
 if TYPE_CHECKING:
     from gemdat import SimulationData, SitesData
@@ -116,5 +117,88 @@ def plot_collective_jumps(*, data: SimulationData, sites: SitesData,
     fig.colorbar(mat, ax=ax)
 
     ax.set(title='Cooperative jumps per jump-type combination')
+
+    return fig
+
+
+def plot_jumps_3d(*, data: SimulationData, sites: SitesData,
+                  **kwargs) -> plt.Figure:
+    """Plot jumps in 3D.
+
+    Parameters
+    ----------
+    data : SimulationData
+        Input simulation data
+    sites : SitesData
+        Input sites data
+
+    Returns
+    -------
+    plt.Figure
+    """
+
+    class LabelItems:
+
+        def __init__(self, labels, coords):
+            self.labels = labels
+            self.coords = coords
+
+        def items(self):
+            yield from zip(self.labels, self.coords)
+
+    coords = sites.structure.frac_coords
+    lattice = data.structure.lattice
+
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+
+    site_labels = LabelItems(sites.structure.labels, coords)
+
+    xyz_labels = LabelItems('OABC', [[-0.1, -0.1, -0.1], [1.1, -0.1, -0.1],
+                                     [-0.1, 1.1, -0.1], [-0.1, -0.1, 1.1]])
+
+    plotter.plot_lattice_vectors(lattice, ax=ax, linewidth=1)
+    plotter.plot_labels(xyz_labels,
+                        lattice=lattice,
+                        ax=ax,
+                        color='green',
+                        size=12)
+    plotter.plot_points(coords, lattice=lattice, ax=ax)
+
+    for i, j in zip(*np.triu_indices(len(coords), k=1)):
+        count = sites.transitions[i, j] + sites.transitions[j, i]
+        if count == 0:
+            continue
+
+        coord_i = coords[i]
+        coord_j = coords[j]
+
+        lw = 1 + np.log(count)
+
+        _, image = lattice.get_distance_and_image(coord_i, coord_j)
+
+        # NOTE: might need to plot `line = [coord_i - image, coord_j]` as well
+        line = [coord_i, coord_j + image]
+
+        plotter.plot_path(line,
+                          lattice=lattice,
+                          ax=ax,
+                          color='red',
+                          linewidth=lw)
+
+    plotter.plot_labels(site_labels,
+                        lattice=lattice,
+                        ax=ax,
+                        color='black',
+                        size=8)
+
+    ax.set(
+        title='Jumps between sites',
+        xlabel="x' (ang)",
+        ylabel="y' (ang)",
+        zlabel="z' (ang)",
+    )
+
+    ax.set_aspect('equal')
 
     return fig
