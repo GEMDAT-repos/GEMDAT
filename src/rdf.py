a23742626fa4cb95452bf4a7e17a16517531fbd3
Stef Smeets <s.smeets@esciencecenter.nl>
2023-07-18 16:55:01 +0200

    Add transition states

--- src/rdf.py
+++ src/rdf.py
@@ -1,15 +1,50 @@
+from collections import defaultdict
+
 import numpy as np
 from gemdat import SimulationData, SitesData
 
 
-def calculate_rdfs(*,
-                   data: SimulationData,
-                   sites: SitesData,
-                   diff_coords: np.ndarray,
-                   n_steps: int,
-                   equilibration_steps: int,
-                   max_dist: float = 5.0,
-                   resolution: float = 0.1) -> dict[str, np.ndarray]:
+def uniqify_labels(arr, labels):
+    unique_labels = list(set(labels))
+    mapping = np.array([-1] + [unique_labels.index(label) for label in labels])
+
+    palette = np.arange(len(labels), dtype=int)
+
+    index = np.digitize(arr, palette, right=True)
+    return mapping[index]
+
+
+def get_states(labels):
+    unique_labels = list(set(labels))
+
+    states = {}
+
+    r = [-1] + list(range(len(unique_labels)))
+
+    for i in r:
+        for j in r:
+            for k in r:
+                if i != -1:
+                    state = '@' + unique_labels[i]
+                elif j == -1 or k == -1:
+                    state = '~>' + unique_labels[j]
+                else:
+                    state = unique_labels[j] + '->' + unique_labels[k]
+
+                states[int(i * 1e6 + j * 1e3 + k)] = state
+
+    return states
+
+
+def calculate_rdfs(
+        *,
+        data: SimulationData,
+        sites: SitesData,
+        diff_coords: np.ndarray,
+        n_steps: int,
+        equilibration_steps: int,
+        max_dist: float = 5.0,
+        resolution: float = 0.1) -> dict[str, dict[str, np.ndarray]]:
     """
     Parameters
     ----------
@@ -33,6 +68,17 @@ def calculate_rdfs(*,
     rdfs : dict[str, np.ndarray]
         Dictionary with rdf arrays per symbol
     """
+    labels = data.structure.labels
+
+    states2str = get_states(labels)
+
+    atom_sites = uniqify_labels(sites.atom_sites, labels)
+    atom_sites_from = uniqify_labels(sites.atom_sites_from, labels)
+    atom_sites_to = uniqify_labels(sites.atom_sites_to, labels)
+
+    states_array = (atom_sites * 1e6 + atom_sites_from * 1e3 +
+                    atom_sites_to).astype(int)
+
     lattice = data.lattice
 
     symbols = data.structure.symbol_set
@@ -45,12 +91,16 @@ def calculate_rdfs(*,
 
     n_symbols = len(symbols)
 
-    bin_width = 0.1
-    bins = np.arange(0, max_dist + bin_width, bin_width)
+    bins = np.arange(0, max_dist + resolution, resolution)
+    length = len(bins) + 1
 
-    rdfs = [np.zeros(len(bins) + 1, dtype=int) for symbol in symbols]
+    rdfs: dict[tuple[str, str],
+               np.ndarray] = defaultdict(lambda: np.zeros(length, dtype=int))
 
     for i in range(n_steps):
+        if i % 1000 == 0:
+            print(i)
+
         all_coords = data.trajectory_coords[equilibration_steps + i]
         diff_coords = diff_coords[i]
 
@@ -58,17 +108,30 @@ def calculate_rdfs(*,
 
         rdf = np.digitize(rdf, bins, right=True)
 
-        for i in range(n_symbols):
-            rdf_s = rdf[:, symbol_indices[i]]
-            rdfs[i] += np.bincount(rdf_s.flatten())
+        states = np.unique(states_array[i], axis=0)
 
-    # Drop last element with distance > max_dist
-    rdfs = [rdf[:-1] for rdf in rdfs]
+        for j in range(n_symbols):
+            for state in states:
+                k_idx = np.argwhere(states_array[i] == state)
 
-    return dict(zip(symbols, rdfs))
+                symbol = symbols[j]
+                state_str = states2str[state]
+
+                rdf_s = rdf[k_idx, symbol_indices[j]]
+
+                rdfs[state_str, symbol] += np.bincount(rdf_s.flatten(),
+                                                       minlength=length)
+
+    new_rdf: dict[str, dict[str, np.ndarray]] = defaultdict(dict)
+
+    for (k0, k1), v in rdfs.items():
+        # Drop last element with distance > max_dist
+        new_rdf[k0][k1] = v[:-1]
+
+    return new_rdf
 
 
-def plot_rdf(rdfs: dict[str, np.ndarray]):
+def plot_rdf(rdfs: dict[str, np.ndarray], name: str | None = None):
     """Plot radial distribution function.
 
     Parameters
@@ -81,9 +144,11 @@ def plot_rdf(rdfs: dict[str, np.ndarray]):
     fig, ax = plt.subplots()
 
     for symbol, rdf in rdfs.items():
-        ax.plot(rdf, label=symbol)
+        ax.plot(rdf[:-1], label=symbol)
+
+    suffix = f' ({name})' if name else ''
 
     ax.legend()
-    ax.set(title='Radial distribution function',
+    ax.set(title=f'Radial distribution function{suffix}',
            xlabel='Distance (Ang)',
            ylabel='Counts')
