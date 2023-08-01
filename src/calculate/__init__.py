f64497e54c168ee8d8cd9fe3df07089b7f0dfad1
Stef Smeets <s.smeets@esciencecenter.nl>
2023-08-01 12:58:32 +0200

    Add import

--- src/calculate/__init__.py
+++ src/calculate/__init__.py
@@ -1,8 +1,10 @@
 from .displacements import Displacements
+from .extras import calculate_all
 from .tracer import Tracer
 from .vibration import Vibration
 
 __all__ = [
+    'calculate_all',
     'Displacements',
     'Vibration',
     'Tracer',
