27849122729ed6e314179fb527d4b8d8df071945
Stef Smeets <s.smeets@esciencecenter.nl>
2023-08-16 10:56:36 +0200

    Add more plots to tests

--- tests/integration/plot_test.py
+++ tests/integration/plot_test.py
@@ -29,23 +29,29 @@ def test_plot_vibrational_amplitudes(vasp_traj):
     plots.plot_vibrational_amplitudes(trajectory=vasp_traj)
 
 
-# @image_comparison2(baseline_images=['jumps_vs_distance'])
-# def test_plot_jumps_vs_distance(vasp_traj):
-#     plots.plot_jumps_vs_distance(trajectory=vasp_traj,
-#                                  sites=sites)
+@image_comparison2(baseline_images=['jumps_vs_distance'])
+def test_plot_jumps_vs_distance(vasp_traj, vasp_sites):
+    plots.plot_jumps_vs_distance(trajectory=vasp_traj, sites=vasp_sites)
 
-# @image_comparison2(baseline_images=['jumps_vs_time'])
-# def test_plot_jumps_vs_time(vasp_traj):
-#     plots.plot_jumps_vs_time(trajectory=vasp_traj, sites=sites)
 
-# @image_comparison2(baseline_images=['collective_jumps'])
-# def test_plot_collective_jumps(vasp_traj):
-#     plots.plot_collective_jumps(trajectory=vasp_traj, sites=sites)
+@image_comparison2(baseline_images=['jumps_vs_time'])
+def test_plot_jumps_vs_time(vasp_traj, vasp_sites):
+    plots.plot_jumps_vs_time(trajectory=vasp_traj, sites=vasp_sites)
 
-# @image_comparison2(baseline_images=['jumps_3d'])
-# def test_plot_jumps_3d(vasp_traj):
-#     plots.plot_jumps_3d(trajectory=vasp_traj, sites=sites)
 
-# @image_comparison2(baseline_images=['jumps_3d_animation'])
-# def test_plot_jumps_3d_animation(vasp_traj):
-#     plots.plot_jumps_3d_animation(trajectory=vasp_traj, sites=sites)
+@image_comparison2(baseline_images=['collective_jumps'])
+def test_plot_collective_jumps(vasp_traj, vasp_sites):
+    plots.plot_collective_jumps(trajectory=vasp_traj, sites=vasp_sites)
+
+
+@image_comparison2(baseline_images=['jumps_3d'])
+def test_plot_jumps_3d(vasp_traj, vasp_sites):
+    plots.plot_jumps_3d(trajectory=vasp_traj, sites=vasp_sites)
+
+
+@image_comparison2(baseline_images=['jumps_3d_animation'])
+def test_plot_jumps_3d_animation(vasp_traj, vasp_sites):
+    plots.plot_jumps_3d_animation(trajectory=vasp_traj,
+                                  sites=vasp_sites,
+                                  t_start=1000,
+                                  t_stop=1001)
