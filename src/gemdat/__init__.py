d57fd573715f6344c583a7d2addb05851f8fb976
Github Action (publish.yaml) <victor@lipsum.eu>
2023-09-12 13:47:34 +0000

    Bump version: 0.0.1 → 0.9.0

--- src/gemdat/__init__.py
+++ src/gemdat/__init__.py
@@ -4,7 +4,7 @@ from .sites import SitesData
 from .trajectory import Trajectory
 from .volume import trajectory_to_volume
 
-__version__ = '0.0.1'
+__version__ = '0.9.0'
 __all__ = [
     'read_cif',
     'load_known_material',
