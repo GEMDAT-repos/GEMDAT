ffe81a55ee5a175360b3a90d8e2d5b827ee9673c
Stef Smeets <s.smeets@esciencecenter.nl>
2023-09-14 14:14:30 +0200

    Add failing test

--- tests/segmentation_test.py
+++ tests/segmentation_test.py
@@ -1,8 +1,10 @@
 import numpy as np
+import pytest
 from gemdat.segmentation import watershed_pbc
 
 
 def test_watershed06():
+    """Test watershed in horizontal direction."""
     data = np.array(
         [
             [1, 1, 1, 1, 1, 1, 1],
@@ -35,6 +37,42 @@ def test_watershed06():
         [1, 1, 1, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0],
+    ])
+
+    np.testing.assert_allclose(out, expected)
+
+
+@pytest.mark.xfail(reason='https://github.com/GEMDAT-repos/GEMDAT/issues/138')
+def test_watershed07():
+    """Test watershed in vertical direction."""
+    data = np.array(
+        [
+            [1, 1, 0, 0, 0, 0, 0, 1, 1],
+            [1, 1, 0, 0, 0, 0, 0, 1, 1],
+            [1, 1, 0, 0, 0, 0, 0, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 0, 0, 0, 0, 0, 1, 1, 1],
+            [1, 0, 0, 0, 0, 0, 1, 1, 1],
+        ],
+        dtype=int,
+    )
+
+    mask = data == 0
+
+    markers = np.zeros_like(data, dtype=int)
+    markers[6, 4] = 1
+
+    out = watershed_pbc(data, markers, mask=mask)
+
+    expected = np.array([
+        [0, 0, 1, 1, 1, 1, 1, 0, 0],
+        [0, 0, 1, 1, 1, 1, 1, 0, 0],
+        [0, 0, 1, 1, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 1, 1, 0, 0, 0],
+        [0, 1, 1, 1, 1, 1, 0, 0, 0],
     ])
 
     np.testing.assert_allclose(out, expected)
