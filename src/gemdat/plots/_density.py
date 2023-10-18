582b92101e92f800ed6106e03d6e5df1368df551
Stef Smeets <s.smeets@esciencecenter.nl>
2023-10-18 16:39:46 +0200

    Add docstring

--- src/gemdat/plots/_density.py
+++ src/gemdat/plots/_density.py
@@ -15,41 +15,76 @@ if TYPE_CHECKING:
 
 
 def plot_lattice_vectors(lattice: Lattice, *, fig: go.Figure):
-    o = lattice.get_cartesian_coords([0.0, 0.0, 0.0])
+    """Plot lattice using plotly.
+
+    Parameters
+    ----------
+    lattice : Lattice
+        Input lattice
+    fig : go.Figure
+        Plotly figure to add traces to
+    """
+    org = lattice.get_cartesian_coords([0.0, 0.0, 0.0])
     a = lattice.get_cartesian_coords([1.0, 0.0, 0.0])
     b = lattice.get_cartesian_coords([0.0, 1.0, 0.0])
     c = lattice.get_cartesian_coords([0.0, 0.0, 1.0])
     abc = a + b + c
 
-    for start, stop in ((o, a), (o, b), (o, c), (a, a + b), (a, a + c),
-                        (b, b + a), (b, b + c), (c, c + a), (c, c + b),
-                        (abc, abc - a), (abc, abc - b), (abc, abc - c)):
+    for i, (start, stop) in enumerate(
+        ((org, a), (org, b), (org, c), (a, a + b), (a, a + c), (b, b + a),
+         (b, b + c), (c, c + a), (c, c + b), (abc, abc - a), (abc, abc - b),
+         (abc, abc - c))):
+
+        color = ('red', 'blue', 'green', 'black')[min(i, 3)]
+        width = (5, 5, 5, 1)[min(i, 3)]
+
         fig.add_trace(
             go.Scatter3d(
                 x=(start[0], stop[0]),
                 y=(start[1], stop[1]),
                 z=(start[2], stop[2]),
                 mode='lines',
-                line={'color': 'black'},
+                name=None,
+                line={
+                    'color': color,
+                    'width': width
+                },
                 showlegend=False,
             ))
 
-    return fig
 
+def plot_points(points: np.ndarray,
+                labels: Sequence,
+                *,
+                fig: go.Figure,
+                point_size: int = 5):
+    """Plot points using plotly.
 
-def plot_points(points: np.ndarray, labels: Sequence, *, fig: go.Figure):
-    point_size = 5
+    Parameters
+    ----------
+    points : np.ndarray
+        Input points
+    labels : Sequence
+        Labels for points. Length must match points.
+    fig : go.Figure
+        Plotly figure to add traces to
+    point_size : int, optional
+        Size of the points
+    """
     colors = px.colors.qualitative.G10
+
+    assert len(points) == len(labels)
 
     for i, (x, y, z) in enumerate(points):
         color = colors[i % len(colors)]
+        label = labels[i]
 
         fig.add_trace(
             go.Scatter3d(x=[x],
                          y=[y],
                          z=[z],
                          mode='markers',
-                         name=labels[i],
+                         name=label,
                          marker={
                              'size': point_size,
                              'color': color,
@@ -58,31 +93,56 @@ def plot_points(points: np.ndarray, labels: Sequence, *, fig: go.Figure):
                              }
                          },
                          showlegend=False))
-    return fig
 
 
 def plot_structure(structure: Structure, *, fig: go.Figure):
+    """Plot structure using plotly.
+
+    Parameters
+    ----------
+    structure : Structure
+        Input structure
+    fig : go.Figure
+        Plotly figure to add traces too
+    """
     plot_points(structure.cart_coords, labels=structure.labels, fig=fig)
     plot_lattice_vectors(structure.lattice, fig=fig)
 
 
-def plot_volume(volume, *, fig):
+def plot_volume(
+    volume: Volume,
+    *,
+    fig: go.Figure,
+    colors: list[str] = ['red', 'yellow', 'cyan'],
+    isovals: list[float] = [0.25, 0.10, 0.007],
+    alphavals: list[float] = [0.6, 0.3, 0.15],
+):
+    """Plot volume using plotly.
+
+    Parameters
+    ----------
+    volume : Volume
+        Input volume
+    fig : go.Figure
+        Plotly figure to add traces too
+    colors : list[str], optional
+        Adjust colors as needed. Length of color, isovals and alphavals must match
+    isovals : list[float], optional
+        Adjust isosurface values as needed
+    alphavals : list[float], optional
+        Adjust transparency as needed
+    """
     data = volume.data
-    # data = np.pad(data, ((0, 1), (0,1), (0,1)), mode='wrap')
     data = gaussian_filter(data, sigma=1.0)
 
-    colors = ['red', 'yellow', 'cyan']
-    iso_values = [0.25, 0.10, 0.007]  # Adjust isosurface values as needed
-    alpha_values = [0.7, 0.5, 0.35]  # Adjust transparency as needed
+    assert len(colors) == len(isovals) == len(alphavals)
 
-    # Plot isosurfaces
-    for i, isoval in enumerate(iso_values):
+    for i, isoval in enumerate(isovals):
         isoval = isoval * np.max(data)
         verts, faces, _, _ = measure.marching_cubes(data, level=isoval)
 
         # Transform verts to cartesian system
         verts = (verts + 0.5) / np.array(data.shape)
-
         cart_verts = volume.lattice.get_cartesian_coords(verts)
 
         fig.add_trace(
@@ -93,22 +153,37 @@ def plot_volume(volume, *, fig):
                       j=faces[:, 1],
                       k=faces[:, 2],
                       name=f'{isoval=}',
-                      opacity=alpha_values[i],
+                      opacity=alphavals[i],
                       color=colors[i],
                       showlegend=False))
 
-    return fig
-
 
 def density(vol: Volume, structure: Structure) -> go.Figure:
+    """Create density plot from volume and structure.
+
+    Uses plotly as plotting backend.
+
+    Arguments
+    ---------
+    vol : Volume
+        Input volume
+    structure : Structure
+        Input structure
+
+    Returns
+    -------
+    fig : go.Figure
+        Output as plotly figure
+    """
     lattice = structure.lattice
 
     fig = go.Figure()
+
     plot_lattice_vectors(lattice, fig=fig)
     plot_points(structure.cart_coords, structure.labels, fig=fig)
     plot_volume(vol, fig=fig)
 
-    fig.update_layout(title='Density of diffusing element',
+    fig.update_layout(title='Density',
                       scene={
                           'aspectmode': 'manual',
                           'aspectratio': {
