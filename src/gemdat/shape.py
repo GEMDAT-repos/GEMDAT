3359229d00424b3ddb521f0cd010199829cbbabf
Stef Smeets <s.smeets@esciencecenter.nl>
2023-11-01 17:09:30 +0100

    Update api

--- src/gemdat/shape.py
+++ src/gemdat/shape.py
@@ -1,17 +1,21 @@
 from __future__ import annotations
 
+from dataclasses import dataclass
+
 import numpy as np
-from pymatgen import symmetry
 from pymatgen.core import Lattice, PeriodicSite, Structure
+from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
+from pymatgen.symmetry.structure import SymmetrizedStructure
 
 from .trajectory import Trajectory
 from .utils import warn_lattice_not_close
 
 
-def plot_cluster(coords: np.ndarray, name: str, bins: int = 50):
+def plot_shape(shape: ShapeData, bins: int = 50):
     import matplotlib.pyplot as plt
 
-    axis_label = ('X / Å', 'Y / Å', 'Z / Å')
+    x_labels = ('X / Å', 'Y / Å', 'Z / Å')
+    y_labels = ('Y / Å', 'Z / Å', 'X / Å')
 
     fig, axes = plt.subplots(nrows=2,
                              ncols=3,
@@ -20,20 +24,21 @@ def plot_cluster(coords: np.ndarray, name: str, bins: int = 50):
                              gridspec_kw={'height_ratios': (4, 1)})
     fig.tight_layout()
 
-    distances = np.sum(coords**2, axis=1)**0.5
+    distances = np.sum(shape.coords**2, axis=1)**0.5
 
     msd = np.mean(distances**2)
     std = np.std(distances**2)
-    title = f'{name}: MSD = {msd:.3f}$~Å^2$, std = {std:.3f}'
+    title = f'{shape.name}: MSD = {msd:.3f}$~Å^2$, std = {std:.3f}'
 
     axes[0, 1].set_title(title)
 
-    for j, (axis_i, axis_j) in enumerate(((0, 1), (1, 2), (2, 0))):
-        ax0 = axes[0, j]
-        ax1 = axes[1, j]
+    for i, (x, y) in enumerate(
+        ((shape.x, shape.y), (shape.y, shape.z), (shape.z, shape.x))):
+        ax0 = axes[0, i]
+        ax1 = axes[1, i]
 
-        ax0.hist2d(x=coords[:, axis_i], y=coords[:, axis_j], bins=bins)
-        ax0.set_ylabel(axis_label[axis_j])
+        ax0.hist2d(x=x, y=y, bins=bins)
+        ax0.set_ylabel(y_labels[i])
 
         circle = plt.Circle((0, 0), msd, color='r', linestyle='--', fill=False)
         ax0.add_patch(circle)
@@ -41,30 +46,51 @@ def plot_cluster(coords: np.ndarray, name: str, bins: int = 50):
         ax0.scatter(x=[0], y=[0], color='r', marker='.')
         ax0.axis('equal')
 
-        ax1.hist(x=coords[:, axis_i], bins=bins, density=True)
-        ax1.set_xlabel(axis_label[axis_i])
+        ax1.hist(x=x, bins=bins, density=True)
+        ax1.set_xlabel(x_labels[i])
         ax1.set_ylabel('density')
 
     fig.tight_layout()
 
 
+@dataclass
+class ShapeData:
+    name: str
+    coords: np.ndarray
+
+    @property
+    def x(self):
+        return self.coords[:, 0]
+
+    @property
+    def y(self):
+        return self.coords[:, 1]
+
+    @property
+    def z(self):
+        return self.coords[:, 2]
+
+
 class ShapeAnalyzer:
 
-    def __init__(self, structure: Structure):
-        self.structure = structure
-        self.sga = symmetry.analyzer.SpacegroupAnalyzer(structure)
+    def __init__(self, symmetrized_structure: SymmetrizedStructure):
+        self.unique_sites = [
+            sites[0] for sites in symmetrized_structure.equivalent_sites
+        ]
+        self.lattice = symmetrized_structure.lattice
+        self.symops = symmetrized_structure.spacegroup
 
-        self.symops = self.sga.get_space_group_operations()
-        symstruct = self.sga.get_symmetrized_structure()
+    @classmethod
+    def from_structure(cls, structure: Structure):
+        sga = SpacegroupAnalyzer(structure)
+        symmetrized_structure = sga.get_symmetrized_structure()
+        return cls(symmetrized_structure=symmetrized_structure)
 
-        self.unique_sites = [sites[0] for sites in symstruct.equivalent_sites]
-        self.lattice = symstruct.lattice
-
-    def set_positions_from_trajectory(self,
-                                      trajectory: Trajectory,
-                                      *,
-                                      supercell: None
-                                      | tuple[float, float, float] = None):
+    def _positions_from_trajectory(self,
+                                   trajectory: Trajectory,
+                                   *,
+                                   supercell: None
+                                   | tuple[float, float, float] = None):
         """Lattices must be similar.
 
         Supercell is used to fold trajectory positions into same
@@ -85,13 +111,13 @@ class ShapeAnalyzer:
 
         self.positions = positions
 
-    def _find_equivalent_positions(self,
-                                   *,
-                                   site: PeriodicSite,
-                                   threshold: float = 1.0):
+    def find_equivalent_positions(self,
+                                  *,
+                                  site: PeriodicSite,
+                                  positions: np.ndarray,
+                                  threshold: float = 1.0):
         lattice = self.lattice
         symops = self.symops
-        positions = self.positions
         unq_coord = site.frac_coords
         cluster = []
 
@@ -117,18 +143,50 @@ class ShapeAnalyzer:
 
             cluster.append(inversed)
 
-        coords = np.vstack(cluster) - unq_coord
+        centered = np.vstack(cluster) - unq_coord
 
         # convert to cartesian
-        return self.lattice.get_cartesian_coords(coords)
+        return self.lattice.get_cartesian_coords(centered)
 
-    def analyze(self, *, threshold: float = 1.0):
-        clusters = {}
+    def analyze_trajectory(self,
+                           *,
+                           trajectory: Trajectory,
+                           supercell: None
+                           | tuple[float, float, float] = None,
+                           threshold: float = 1.0) -> list[ShapeData]:
+        """Lattices must be similar.
+
+        Supercell is used to fold trajectory positions into same
+        lattice.
+        """
+        test_lattice = trajectory.get_lattice(0)
+        positions = trajectory.positions.reshape(-1, 3)
+
+        if supercell is not None:
+            scale_arr = np.array(supercell)
+
+            scale_matrix = (1 / scale_arr) * np.eye(3)
+            test_lattice = Lattice(np.dot(scale_matrix, test_lattice.matrix))
+
+            positions = np.mod(positions, 1 / scale_arr) * scale_arr
+
+        warn_lattice_not_close(self.lattice, test_lattice)
+
+        return self.analyze_positions(positions=positions, threshold=threshold)
+
+    def analyze_positions(self,
+                          *,
+                          positions: np.ndarray,
+                          threshold: float = 1.0) -> list[ShapeData]:
+        shapes = []
 
         for site in self.unique_sites:
             print(f'\n{site.label}: {site.frac_coords}\n')
 
-            clusters[site.label] = self._find_equivalent_positions(
-                site=site, threshold=threshold)
+            eqv_coords = self.find_equivalent_positions(site=site,
+                                                        positions=positions,
+                                                        threshold=threshold)
 
-        return clusters
+            shapes.append(ShapeData(name=site.label, coords=eqv_coords))
+
+        return shapes
