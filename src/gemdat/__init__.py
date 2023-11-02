76f9eb22247b99205434dd7750c0a480227d84ad
Stef Smeets <s.smeets@esciencecenter.nl>
2023-11-02 15:51:05 +0100

    Add ShapeAnalyzer to api

--- src/gemdat/__init__.py
+++ src/gemdat/__init__.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from .io import load_known_material, read_cif
+from .shape import ShapeAnalyzer
 from .simulation_metrics import SimulationMetrics
 from .sites import SitesData
 from .trajectory import Trajectory
@@ -10,6 +11,7 @@ __version__ = '0.9.3'
 __all__ = [
     'read_cif',
     'load_known_material',
+    'ShapeAnalyzer',
     'SimulationMetrics',
     'SitesData',
     'Trajectory',
