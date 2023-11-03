ab5e5931c88c4d48772ab94fb170649632bbdd28
Victor Azizi <victor@lipsum.eu>
2023-11-03 15:12:56 +0100

    Fixed matplotlib imports

---
+++ src/gemdat/plots/matplotlib/__init__.py
@@ -0,0 +1,35 @@
+"""This module contains all the plots that Gemdat can generate with
+matplotlib."""
+from __future__ import annotations
+
+from ._displacements import (
+    displacement_histogram,
+    displacement_per_element,
+    displacement_per_site,
+)
+from ._jumps import (
+    collective_jumps,
+    jumps_3d,
+    jumps_3d_animation,
+    jumps_vs_distance,
+    jumps_vs_time,
+)
+from ._rdf import radial_distribution
+from ._vibration import (
+    frequency_vs_occurence,
+    vibrational_amplitudes,
+)
+
+__all__ = [
+    'displacement_per_site',
+    'displacement_per_element',
+    'displacement_histogram',
+    'frequency_vs_occurence',
+    'vibrational_amplitudes',
+    'jumps_vs_distance',
+    'jumps_vs_time',
+    'collective_jumps',
+    'jumps_3d',
+    'jumps_3d_animation',
+    'radial_distribution',
+]
