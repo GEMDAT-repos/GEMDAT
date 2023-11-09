28d6259c7803b343145581e0bac930fb7cec69c6
Stef Smeets <s.smeets@esciencecenter.nl>
2023-11-09 11:54:10 +0100

    Re-organize imports

--- src/gemdat/plots/__init__.py
+++ src/gemdat/plots/__init__.py
@@ -1,29 +1,26 @@
 """This module contains all the plots that Gemdat can generate."""
 from __future__ import annotations
 
-from .matplotlib import shape
-
 # Matplotlib plots
-from .matplotlib._displacements import displacement_per_site
-from .matplotlib._jumps import (
+from .matplotlib import (
     collective_jumps,
+    displacement_per_site,
+    frequency_vs_occurence,
     jumps_3d,
     jumps_3d_animation,
+    radial_distribution,
+    shape,
 )
-from .matplotlib._rdf import radial_distribution
-from .matplotlib._vibration import frequency_vs_occurence
 
 # Plotly plots (matplotlib version might be available)
-from .plotly._density import density
-from .plotly._displacements import (
+from .plotly import (
+    density,
     displacement_histogram,
     displacement_per_element,
-)
-from .plotly._jumps import (
     jumps_vs_distance,
     jumps_vs_time,
+    vibrational_amplitudes,
 )
-from .plotly._vibration import vibrational_amplitudes
 
 __all__ = [
     'collective_jumps',
---
+++ src/gemdat/plots/plotly/__init__.py
@@ -0,0 +1,22 @@
+"""This module contains all the plots that Gemdat can generate with plotly."""
+from __future__ import annotations
+
+from ._density import density
+from ._displacements import (
+    displacement_histogram,
+    displacement_per_element,
+)
+from ._jumps import (
+    jumps_vs_distance,
+    jumps_vs_time,
+)
+from ._vibration import vibrational_amplitudes
+
+__all__ = [
+    'density',
+    'displacement_histogram',
+    'displacement_per_element',
+    'jumps_vs_distance',
+    'jumps_vs_time',
+    'vibrational_amplitudes',
+]
