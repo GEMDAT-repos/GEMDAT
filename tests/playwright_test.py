9c50918f8abf8478cab2e4c293c0dd6e8edcbd90
Victor Azizi <victor@lipsum.eu>
2023-11-14 15:04:08 +0100

    Update gemdash tests (#180)
    * updated ruff and mypy pre-commit version
    
    * remove iffy plot checks
    
    * removed iffy checkbox

--- tests/playwright_test.py
+++ tests/playwright_test.py
@@ -30,49 +30,24 @@ def before_module():
     p.kill()
 
 
-@pytest.mark.skip(reason='https://github.com/GEMDAT-repos/GEMDAT/issues/173')
 def test_gemdash(page: Page):
     # Goto web page
     page.goto(BASE_URL)
 
-    # Enter vasprun location
     page.get_by_label('filename').click()
     page.get_by_label('filename').fill(
         'tests/data/short_simulation/vasprun.xml')
     page.get_by_label('filename').press('Enter')
 
-    # this will take a wile, increase timeout on next check
-
-    # Correct SuperCell
-    expect(page.get_by_label('supercell x')).to_be_visible(timeout=100_000)
-    page.get_by_label('supercell x').click()
-    page.get_by_label('supercell x').fill('2')
-    page.get_by_label('supercell x').press('Enter')
-
-    # Check if pictures are present
-    expect(page.locator('img').first).to_be_visible()
-
-    # Enable RDFs
-    page.get_by_role('tab', name='RDF plots').click()
-    page.get_by_test_id('stCheckbox').locator('span').click()
-
-    # Check pictures
-    expect(page.get_by_role('img',
-                            name='0').first).to_be_visible(timeout=100_000)
-    expect(page.get_by_role('img', name='0').nth(1)).to_be_visible()
-    expect(page.get_by_role('img', name='0').nth(2)).to_be_visible()
-
-    # change equilibration steps
-    page.get_by_label('Equilibration Steps').click()
-    page.get_by_label('Equilibration Steps').fill('100')
-    page.get_by_label('Equilibration Steps').press('Enter')
-
-    # check fullscreen
-
-    # These values should be presented on the page
-    expect(page.get_by_text('0.511089')).to_be_visible(timeout=100_000)
-    expect(page.get_by_text('40.7774')).to_be_visible()
-    expect(page.get_by_text('119.858')).to_be_visible()
-    expect(page.get_by_text('1.70636e-09')).to_be_visible()
-    expect(page.get_by_text('(8.5+/-0.7)e+12')).to_be_visible()
+    # check values
+    expect(page.get_by_text('1.57061e-09')).to_be_visible(timeout=60_000)
+    expect(page.get_by_text('110.322')).to_be_visible()
     expect(page.get_by_text('2.45567e+28')).to_be_visible()
+    expect(page.get_by_text('(8.5+/-0.9)e+12')).to_be_visible()
+    expect(page.get_by_text('0.52043')).to_be_visible()
+    expect(page.get_by_text('40.7774')).to_be_visible()
+
+    page.get_by_role('tab', name='RDF plots').click()
+    page.locator('label').filter(has_text='Plot RDFs').locator('span').click()
+
+    page.get_by_role('tab', name='Density plots').click()
--- tools/flake.nix
+++ tools/flake.nix
@@ -1,11 +1,12 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   inputs.ruff_287.url = "github:nixos/nixpkgs/e32a63389adb6ee017f8b344e21c80432bb75c10";
+  inputs.ruff_0_1_5.url = "github:nixos/nixpkgs/aa1d7f6320c32010a990ba6c78fcb24cf9e99270";
 
-  outputs = { self, nixpkgs, ruff_287, ... }:
+  outputs = { self, nixpkgs, ruff_0_1_5, ... }:
     let
     pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    ruffpkg = ruff_287.legacyPackages.x86_64-linux.ruff;
+    ruffpkg = ruff_0_1_5.legacyPackages.x86_64-linux.ruff;
     mypython = pkgs.python311;
     pythonpkgs = pkgs.python311Packages;
     in with pkgs; {
