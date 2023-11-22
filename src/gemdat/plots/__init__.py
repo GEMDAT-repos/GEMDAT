7799cbd5b62867eab0c35ed8c88d6475331580d6
Anastasia K. Lavrinenko <75916717+AstyLavrinenko@users.noreply.github.com>
2023-11-22 10:59:50 +0100

    Remove double mention

--- src/gemdat/plots/__init__.py
+++ src/gemdat/plots/__init__.py
@@ -38,7 +38,6 @@ __all__ = [
     'jumps_vs_distance',
     'jumps_vs_time',
     'msd_per_element',
-    'path_on_landscape',
     'radial_distribution',
     'shape',
     'vibrational_amplitudes',
