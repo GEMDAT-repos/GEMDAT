9f18c2b9d8df5d96ab6bba1ecf621bcaff631fc0
Stef Smeets <s.smeets@esciencecenter.nl>
2024-01-24 11:49:22 +0100

    Remove site occupancy (handled by transitions) and atom_locations_parts

--- src/gemdat/sites.py
+++ src/gemdat/sites.py
@@ -9,14 +9,12 @@ from typing import Optional
 import numpy as np
 from pymatgen.core import Structure
 
-from .caching import weak_lru_cache
 from .simulation_metrics import SimulationMetrics
-from .transitions import Transitions
 from .utils import is_lattice_similar
 
 if typing.TYPE_CHECKING:
-
     from gemdat.trajectory import Trajectory
+    from gemdat.transitions import Transitions
 
 NOSITE = -1
 
@@ -96,48 +94,7 @@ class SitesData:
         site_pairs = product(labels, repeat=2)
         return [pair for pair in site_pairs]  # type: ignore
 
-    def site_occupancy_parts(
-            self, transitions: Transitions) -> list[dict[str, float]]:
-        """Return [site occupancy][gemdat.sites.SitesData.site_occupancy] dicts
-        per part."""
-        labels = self.site_labels
-        n_steps = len(self.trajectory)
-
-        parts = transitions.split(self.n_parts)
-
-        return [
-            _calculate_site_occupancy(occupancy=part.occupancy(),
-                                      labels=labels,
-                                      n_steps=int(n_steps / self.n_parts))
-            for part in parts
-        ]
-
-    @weak_lru_cache()
-    def atom_locations_parts(
-            self, transitions: Transitions) -> list[dict[str, float]]:
-        """Return [atom locations][gemdat.sites.SitesData.atom_locations] dicts
-        per part."""
-        multiplier = self.n_sites / self.n_floating
-        return [{
-            k: v * multiplier
-            for k, v in part.items()
-        } for part in self.site_occupancy_parts(transitions)]
-
-    def site_occupancy(self, transitions: Transitions):
-        """Calculate percentage occupancy per unique site.
-
-        Returns
-        -------
-        site_occopancy : dict[str, float]
-            Percentage occupancy per unique site
-        """
-        labels = self.site_labels
-        n_steps = len(self.trajectory)
-        return _calculate_site_occupancy(occupancy=transitions.occupancy(),
-                                         labels=labels,
-                                         n_steps=n_steps)
-
-    def atom_locations(self, transitions):
+    def atom_locations(self, transitions: Transitions):
         """Calculate fraction of time atoms spent at a type of site.
 
         Returns
@@ -146,45 +103,15 @@ class SitesData:
             Return dict with the fraction of time atoms spent at a site
         """
         multiplier = self.n_sites / self.n_floating
-        return {
-            k: v * multiplier
-            for k, v in self.site_occupancy(transitions).items()
-        }
 
+        compositions_by_label = defaultdict(list)
 
-def _calculate_site_occupancy(
-    *,
-    occupancy: dict[int, int],
-    labels: list[str],
-    n_steps: int,
-) -> dict[str, float]:
-    """Calculate percentage occupancy per unique site.
+        for site in transitions.occupancy():
+            compositions_by_label[site.label].append(site.composition)
 
-    Parameters
-    ----------
-    occupancy : dict[int, int]
-        Occupancy dict
-    labels : list[str]
-        Site labels
-    n_steps : int
-        Number of steps in time series
+        ret = {}
 
-    Returns
-    -------
-    dict[str, float]
-        Percentage occupancy per unique site
-    """
-    counts = defaultdict(list)
+        for k, v in compositions_by_label.items():
+            ret[k] = sum(v) * multiplier
 
-    assert all(v >= 0 for v in occupancy)
-
-    for k, v in occupancy.items():
-        label = labels[k]
-        counts[label].append(v)
-
-    site_occupancies = {
-        k: sum(v) / (n_steps * labels.count(k))
-        for k, v in counts.items()
-    }
-
-    return site_occupancies
+        return ret
