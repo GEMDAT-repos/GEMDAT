64838ecd531d9e2d97852b8a4d47b73cfbe976c4
SCiarella <simoneciarella@gmail.com>
2024-02-06 15:58:38 +0100

    Rebase and initial cleanup

--- src/gemdat/gemdat_rotation.py
+++ src/gemdat/gemdat_rotation.py
@@ -1,113 +1,119 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Nov 25 11:47:32 2023
+from __future__ import annotations
 
-@author: Matt Damon
-"""
-
-import numpy as np  
 import matplotlib.pyplot as plt
+import numpy as np
 
-def bond_direct(traj, cent, sat, nr_cen , nr_lig = 4, norm=True):
-    '''returns trajectories of normalized unit vectors defined as the distance
+
+def bond_direct(traj, cent, sat, nr_cen, nr_lig=4, norm=True):
+    """Returns trajectories of normalized unit vectors defined as the distance
     between a central and satellite atoms, meant to track orientation of
-    moleules or clusters'''
-    
+    moleules or clusters."""
+
     # Create new trajectory files
     diff_trajectory_sat = traj.filter(sat)
     diff_trajectory_cent = traj.filter(cent)
     nr_ligands = nr_lig
     nr_central_atoms = nr_cen
-    
+
     #fractional coordinates of central atom and sattelite atoms
     frac_coord_sat = diff_trajectory_sat.positions
     frac_coord_cent = diff_trajectory_cent.positions
-    
-    
+
     # Find starting coordinates of all atoms
     central_start_coord = frac_coord_cent[1, :, :]
     sattelite_start_coord = frac_coord_sat[1, :, :]
-    
+
     # Calculate distances between every central atom and all satellite atoms
     lattice = traj.lattice
-    
+
     #function to calculate distance between two positions taking BC intro account
     def calc_dist(frac1, frac2, lattice):
         frac = np.subtract(frac2, frac1)
-        
+
         # Use numpy's modulo operation to handle wrapping
         frac = np.mod(frac + 0.5, 1) - 0.5
-    
+
         cart = np.dot(frac, lattice)
         dist = np.linalg.norm(cart)
         return dist
-    
-    distance = np.array([[calc_dist(central, satellite, lattice) for satellite in sattelite_start_coord] for central in central_start_coord])
-    
+
+    distance = np.array([[
+        calc_dist(central, satellite, lattice)
+        for satellite in sattelite_start_coord
+    ] for central in central_start_coord])
+
     # Determine which satellite atoms are close enough to central atom to be connected
     match_criteria = 1.5 * np.min(distance)
     distance_match = np.where(distance < match_criteria, distance, 0)
-    
+
     # Create array which contains every central atom at rows and associated satellite atoms in the columns
     matching_matrix = np.zeros((len(frac_coord_cent[0, :, 0]), 4), dtype=int)
-    
+
     for k in range(len(frac_coord_cent[0, :, 0])):
         matching_matrix[k, :] = np.where(distance_match[k, :] != 0)[0][:4]
-    
+
     # Get central atoms and satellite atoms in a nice matrix for further calculations
     index_central_atoms = np.arange(nr_central_atoms)
-    combinations = np.array([(i, j) for i in index_central_atoms for j in matching_matrix[i, :]])
-    
+    combinations = np.array([(i, j) for i in index_central_atoms
+                             for j in matching_matrix[i, :]])
+
     # Get all fractional coordinate vectors going from central atom to its 4 ligands
-    direction = frac_coord_sat[:, combinations[:, 1], :] - frac_coord_cent[:, combinations[:, 0], :]
-    
+    direction = frac_coord_sat[:,
+                               combinations[:,
+                                            1], :] - frac_coord_cent[:,
+                                                                     combinations[:,
+                                                                                  0], :]
+
     # Take the periodic boundary conditions into account
     direction = np.where(direction > 0.5, direction - 1, direction)
     direction = np.where(direction < -0.5, direction + 1, direction)
-    
+
     # Create a directions matrix with cartesian vectors
     # direct_cart = np.matmul(lattice.T, np.swapaxes(direction, 2, 1))
     direct_cart = np.matmul(direction, lattice)
-    
+
     if norm:
-        normalized_direct_cart = direct_cart / np.linalg.norm(direct_cart, axis=-1, keepdims=True)
+        normalized_direct_cart = direct_cart / np.linalg.norm(
+            direct_cart, axis=-1, keepdims=True)
         return normalized_direct_cart
     else:
         return direct_cart
 
+
 def cart2sph(x, y, z):
-    '''transform cartesian coordinates to spherical coordinates'''
+    """Transform cartesian coordinates to spherical coordinates."""
     r = np.sqrt(x**2 + y**2 + z**2)
     el = np.arcsin(z / r)
     az = np.arctan2(y, x)
     return az, el, r
 
-def cartesian_to_spherical(direct_cart, degrees = True):
-   '''trajectory from cartesian coordinates to spherical coordinates'''
-   x = direct_cart[:, :, 0]
-   y = direct_cart[:, :, 1]
-   z = direct_cart[:, :, 2]
-    
-   az, el, r = cart2sph(x, y, z)
-    
-   if degrees:
+
+def cartesian_to_spherical(direct_cart, degrees=True):
+    """Trajectory from cartesian coordinates to spherical coordinates."""
+    x = direct_cart[:, :, 0]
+    y = direct_cart[:, :, 1]
+    z = direct_cart[:, :, 2]
+
+    az, el, r = cart2sph(x, y, z)
+
+    if degrees:
         az = np.degrees(az)
         el = np.degrees(el)
-    
-   # Stack the results along the last axis to match the shape of direction_spherical
-   direction_spherical = np.stack((az, el, r), axis=-1)
-    
-   return direction_spherical
 
-def sph_prob(direction_spherical_deg, shape =(360,180)):
+    # Stack the results along the last axis to match the shape of direction_spherical
+    direction_spherical = np.stack((az, el, r), axis=-1)
 
-    az = direction_spherical_deg[:,:,0].flatten()
-    el = direction_spherical_deg[:,:,1].flatten()
-    
+    return direction_spherical
+
+
+def sph_prob(direction_spherical_deg, shape=(360, 180)):
+
+    az = direction_spherical_deg[:, :, 0].flatten()
+    el = direction_spherical_deg[:, :, 1].flatten()
+
     # Compute the 2D histogram - for reasons, x-y inversed
     hist, xedges, yedges = np.histogram2d(el, az, shape)
-    
-    
+
     def calculate_spherical_areas(shape, radius=1):
         azimuthal_angles = np.linspace(0, 360, shape[0])
         elevation_angles = np.linspace(0, 180, shape[1])
@@ -119,41 +125,43 @@ def sph_prob(direction_spherical_deg, shape =(360,180)):
                 azimuthal_increment = np.deg2rad(1)
                 elevation_increment = np.deg2rad(1)
 
-                areas[i, j] = (radius**2) * azimuthal_increment * np.sin(np.deg2rad(elevation_angles[j])) * elevation_increment
+                areas[i, j] = (radius**2) * azimuthal_increment * np.sin(
+                    np.deg2rad(elevation_angles[j])) * elevation_increment
                 #hacky way to get rid of singularity on poles
-                areas[:,0] = areas[:,-1]
+                areas[:, 0] = areas[:, -1]
         return areas
-    
-    areas= calculate_spherical_areas(shape)
-        
+
+    areas = calculate_spherical_areas(shape)
+
     hist = np.divide(hist, areas)
-    
+
     #replace values at the poles where normalization breaks - hacky
-    hist[:,0]=hist[:,1]
-    hist[:,-1]=hist[:,-2]
+    hist[:, 0] = hist[:, 1]
+    hist[:, -1] = hist[:, -2]
     return hist
 
+
 def rectilinear_plot(grid):
-    '''plot a rectilinear projection of a spherical function'''
+    """Plot a rectilinear projection of a spherical function."""
     values = grid.T
-    phi = np.linspace(0, 360, np.ma.size(values,0))
-    theta = np.linspace(0, 180, np.ma.size(values,1))
-     
-    theta, phi = np.meshgrid(theta,phi)
-    
-    
+    phi = np.linspace(0, 360, np.ma.size(values, 0))
+    theta = np.linspace(0, 180, np.ma.size(values, 1))
+
+    theta, phi = np.meshgrid(theta, phi)
+
     fig, ax = plt.subplots(subplot_kw=dict(projection='rectilinear'))
-    cs= ax.contourf(phi, theta, values, cmap='viridis')
-    ax.set_yticks(np.arange(0,190,45))
-    ax.set_xticks(np.arange(0,370,45))  
-    
+    cs = ax.contourf(phi, theta, values, cmap='viridis')
+    ax.set_yticks(np.arange(0, 190, 45))
+    ax.set_xticks(np.arange(0, 370, 45))
+
     ax.set_xlabel(r'azimuthal angle φ $[\degree$]')
     ax.set_ylabel(r'elevation θ $[\degree$]')
-    
+
     ax.grid(visible=True)
-    cbar = fig.colorbar(cs, label= 'areal probability', format='')
-    
+    cbar = fig.colorbar(cs, label='areal probability', format='')
+
     # Rotate the colorbar label by 180 degrees
-    cbar.ax.yaxis.set_label_coords(2.5, 0.5)  # Adjust the position of the label
+    cbar.ax.yaxis.set_label_coords(2.5,
+                                   0.5)  # Adjust the position of the label
     cbar.set_label('areal probability', rotation=270, labelpad=15)
-    return 
+    return
--- src/gemdat/test4.py
+++ src/gemdat/test4.py
@@ -1,26 +1,31 @@
-import matplotlib.pyplot as plt
-from gemdat import Trajectory
-import numpy as np
+from __future__ import annotations
+
 import time
+
 import gemdat_rotation as gr
+import matplotlib.pyplot as plt
+import numpy as np
+
+from gemdat import Trajectory
 
 #%%
-start_time= time.time()
-traj = Trajectory.from_vasprun("vasprun.xml")
+start_time = time.time()
+traj = Trajectory.from_vasprun('vasprun.xml')
 
 end_time = time.time()
 elapsed_time = end_time - start_time
-print(f"Loaded .xml in  {elapsed_time} seconds")
+print(f'Loaded .xml in  {elapsed_time} seconds')
 
 #%%
 
-normalized_direct_cart = gr.bond_direct(traj, "S", "O",  8)
+normalized_direct_cart = gr.bond_direct(traj, 'S', 'O', 8)
 
 #%%
 # Matrix to transform primitive unit cell coordinates to conventional unit cell coordinates
-prim_to_conv_matrix = np.array([[1/np.sqrt(2), -1/np.sqrt(6), 1/np.sqrt(3)],
-                                [1/np.sqrt(2), 1/np.sqrt(6), -1/np.sqrt(3)],
-                                [0, 2/np.sqrt(6), 1/np.sqrt(3)]])
+prim_to_conv_matrix = np.array(
+    [[1 / np.sqrt(2), -1 / np.sqrt(6), 1 / np.sqrt(3)],
+     [1 / np.sqrt(2), 1 / np.sqrt(6), -1 / np.sqrt(3)],
+     [0, 2 / np.sqrt(6), 1 / np.sqrt(3)]])
 
 # Transfrom direct_cart to conventional form
 direct_cart_TRANS = np.matmul(normalized_direct_cart, prim_to_conv_matrix.T)
@@ -48,15 +53,13 @@ sym_ops_Oh_firstrow = np.array([
     np.array([[0, 0, 1], [0, 1, 0], [1, 0, 0]])
 ])
 
-
-
-
 # Multiply row by column to get 8x6 matrix of transformation matrices
-sym_ops_Oh = np.zeros((8,6,3,3))
+sym_ops_Oh = np.zeros((8, 6, 3, 3))
 
 for i in range(len(sym_ops_Oh_firstcolumn)):
     for j in range(len(sym_ops_Oh_firstrow)):
-        sym_ops_Oh[i,j,:,:] = np.matmul(sym_ops_Oh_firstcolumn[i,:,:], sym_ops_Oh_firstrow[j,:,:])
+        sym_ops_Oh[i, j, :, :] = np.matmul(sym_ops_Oh_firstcolumn[i, :, :],
+                                           sym_ops_Oh_firstrow[j, :, :])
 
 # sym_ops_Oh = sym_ops_Oh.reshape(3, 3, -1)
 #%%
@@ -64,51 +67,51 @@ for i in range(len(sym_ops_Oh_firstcolumn)):
 sym_ops_Oh = sym_ops_Oh.reshape(3, 3, -1)
 #%%
 # Apply symmetry elements of Oh space group with vectorized operations
-direction_cart = direct_cart_TRANS[1000:2000,:,:]
+direction_cart = direct_cart_TRANS[1000:2000, :, :]
 
-start_time= time.time()
+start_time = time.time()
 
 n_ts = direction_cart.shape[0]
 n_bonds = direction_cart.shape[1]
 n_symops = sym_ops_Oh.shape[2]
 
-direction_cart_symOh=np.zeros((n_ts,n_bonds * n_symops,3))
-for m in range(n_ts):   
+direction_cart_symOh = np.zeros((n_ts, n_bonds * n_symops, 3))
+for m in range(n_ts):
     for l in range(n_bonds):
         for k in range(n_symops):
-            direction_cart_symOh[m,l*n_symops+k,:] = np.matmul(sym_ops_Oh[:,:,k], direction_cart[m,l,:])
-
+            direction_cart_symOh[m, l * n_symops + k, :] = np.matmul(
+                sym_ops_Oh[:, :, k], direction_cart[m, l, :])
 
 # direction_cart_symOh =  direction_cart_symOh.reshape(n_ts,-1,3)
 
 end_time = time.time()
 elapsed_time = end_time - start_time
-print(f"symmetrized direct cart  {elapsed_time} seconds")
+print(f'symmetrized direct cart  {elapsed_time} seconds')
 
 #%%
 
-m=0
-l=30
-k=46
+m = 0
+l = 30
+k = 46
 
-test = np.matmul(sym_ops_Oh[:,:,k], direction_cart[m,l,:])
+test = np.matmul(sym_ops_Oh[:, :, k], direction_cart[m, l, :])
 
 print(test)
 
-print(direction_cart_symOh[m,(l+1)*(k+1)-1,:])
-
+print(direction_cart_symOh[m, (l + 1) * (k + 1) - 1, :])
 
 #%%
-direction_spherical_deg = gr.cartesian_to_spherical(direction_cart_symOh, degrees=True)
+direction_spherical_deg = gr.cartesian_to_spherical(direction_cart_symOh,
+                                                    degrees=True)
 
 import gemdat_rotation as gr
 
-shape=(90,360)
-az = direction_spherical_deg[:,:,0].flatten()
-el = direction_spherical_deg[:,:,1].flatten()
+shape = (90, 360)
+az = direction_spherical_deg[:, :, 0].flatten()
+el = direction_spherical_deg[:, :, 1].flatten()
 
 # Compute the 2D histogram - for reasons, x-y inversed
-hist, xedges, yedges = np.histogram2d(el, az , shape)
+hist, xedges, yedges = np.histogram2d(el, az, shape)
 
 
 def calculate_spherical_areas(shape, radius=1):
@@ -122,32 +125,32 @@ def calculate_spherical_areas(shape, radius=1):
             azimuthal_increment = np.deg2rad(1)
             elevation_increment = np.deg2rad(1)
 
-            areas[j,i] = (radius**2) * azimuthal_increment * np.sin(np.deg2rad(elevation_angles[j])) * elevation_increment
+            areas[j, i] = (radius**2) * azimuthal_increment * np.sin(
+                np.deg2rad(elevation_angles[j])) * elevation_increment
             #hacky way to get rid of singularity on poles
-            areas[0,:] = areas[-1,0]
+            areas[0, :] = areas[-1, 0]
     return areas
 
-areas= calculate_spherical_areas(shape)
-    
+
+areas = calculate_spherical_areas(shape)
+
 h_norm = np.divide(hist, areas)
 
 #replace values at the poles where normalization breaks - hacky
-h_norm[0,:] = h_norm[1,:]
-h_norm[-1,:] = h_norm[-2,:]
-
+h_norm[0, :] = h_norm[1, :]
+h_norm[-1, :] = h_norm[-2, :]
 
 # hist = gr.sph_prob(direction_spherical_deg)
 plt.close('all')
 gr.rectilinear_plot(hist)
 gr.rectilinear_plot(h_norm)
 
-
 #%%
 plt.figure()
-from scipy.stats import skewnorm
 from scipy.optimize import curve_fit
+from scipy.stats import skewnorm
 
-r = direction_spherical_deg[:,:,2].flatten()
+r = direction_spherical_deg[:, :, 2].flatten()
 data = r
 
 # Specify the number of bins
@@ -157,18 +160,26 @@ bins = 1000
 hist, edges = np.histogram(data, bins=bins, density=True)
 bin_centers = (edges[:-1] + edges[1:]) / 2
 
+
 # Fit a skewed Gaussian distribution to the data
 def skewed_gaussian(x, loc, scale, skew):
     return skewnorm.pdf(x, a=skew, loc=loc, scale=scale)
 
-params, covariance = curve_fit(skewed_gaussian, bin_centers, hist, p0=[1.5, 1, 1.5])
+
+params, covariance = curve_fit(skewed_gaussian,
+                               bin_centers,
+                               hist,
+                               p0=[1.5, 1, 1.5])
 
 # Plot the histogram
 plt.hist(data, bins=bins, density=True, color='blue', alpha=0.7, label='Data')
 
 # Plot the fitted skewed Gaussian distribution
 x_fit = np.linspace(min(bin_centers), max(bin_centers), 1000)
-plt.plot(x_fit, skewed_gaussian(x_fit, *params), 'r-', label='Skewed Gaussian Fit')
+plt.plot(x_fit,
+         skewed_gaussian(x_fit, *params),
+         'r-',
+         label='Skewed Gaussian Fit')
 
 plt.xlabel(r'bond length $[\AA]$')
 plt.ylabel(r'probability density $[\AA^{-1}]$')
@@ -179,90 +190,100 @@ plt.show()
 #%%
 #calculate vector autocorr
 
+
 def autocorr(direct_cart, Npt=100):
     start_time = time.time()
-    
-    Nts=len(normalized_direct_cart) # number timesteps
-    ep=Nts-1    
-    dts= np.round(np.logspace(1, np.log10(ep), Npt)).astype(int)
-    
-    mean_autocorr=np.zeros(len(dts))
-    
+
+    Nts = len(normalized_direct_cart)  # number timesteps
+    ep = Nts - 1
+    dts = np.round(np.logspace(1, np.log10(ep), Npt)).astype(int)
+
+    mean_autocorr = np.zeros(len(dts))
+
     for k, dt in enumerate(dts):
-        autocorr = np.sum(normalized_direct_cart[:-dt] * normalized_direct_cart[dt:], axis=-1)
+        autocorr = np.sum(normalized_direct_cart[:-dt] *
+                          normalized_direct_cart[dt:],
+                          axis=-1)
         mean_autocorr[k] = np.mean(autocorr)
-    
-    
-    
+
     end_time = time.time()
     elapsed_time = end_time - start_time
-    print(f"Autocorrelation function calculated in  {elapsed_time} seconds")
-    
-    return dts*0.002, mean_autocorr #time in ps
+    print(f'Autocorrelation function calculated in  {elapsed_time} seconds')
 
-x,y = autocorr(normalized_direct_cart)
+    return dts * 0.002, mean_autocorr  #time in ps
 
-plt.scatter(x,y)
+
+x, y = autocorr(normalized_direct_cart)
+
+plt.scatter(x, y)
 #%%\
-    
+
+
 def autocorr_par(direct_cart, Npt=100):
     from concurrent.futures import ThreadPoolExecutor
-    
+
     # Assuming normalized_direct_cart is already defined
-    
+
     def compute_autocorr(dt):
-        autocorr = np.sum(normalized_direct_cart[:-dt] * normalized_direct_cart[dt:], axis=-1)
+        autocorr = np.sum(normalized_direct_cart[:-dt] *
+                          normalized_direct_cart[dt:],
+                          axis=-1)
         return np.mean(autocorr)
-    
+
     start_time = time.time()
-    
+
     Nts = len(normalized_direct_cart)
     ep = Nts - 1
     dts = np.round(np.logspace(1, np.log10(ep), Npt)).astype(int)
-    
+
     mean_autocorr = np.zeros(len(dts))
-    
+
     with ThreadPoolExecutor() as executor:
         mean_autocorr = list(executor.map(compute_autocorr, dts))
-    
+
     end_time = time.time()
     elapsed_time = end_time - start_time
-    print(f"Autocorrelation function calculated in {elapsed_time} seconds")
-    
-    return dts*0.002, mean_autocorr
+    print(f'Autocorrelation function calculated in {elapsed_time} seconds')
 
-x,y = autocorr_par(normalized_direct_cart)
+    return dts * 0.002, mean_autocorr
 
-plt.scatter(x,y)
+
+x, y = autocorr_par(normalized_direct_cart)
+
+plt.scatter(x, y)
 
 #%%
 
-import numpy as np
-import matplotlib.pyplot as plt
 from concurrent.futures import ThreadPoolExecutor
 
+import matplotlib.pyplot as plt
+import numpy as np
+
+
 def autocorr_par(direct_cart, Npt=100):
+
     def compute_autocorr(dt):
         autocorr = np.sum(direct_cart[:-dt] * direct_cart[dt:], axis=-1)
         mean_autocorr = np.mean(autocorr)
         std_autocorr = np.std(autocorr)
         return mean_autocorr, std_autocorr
-    
+
     Nts = len(direct_cart)
     ep = Nts - 1
     dts = np.round(np.logspace(1, np.log10(ep), Npt)).astype(int)
-    
+
     mean_autocorr = np.zeros(len(dts))
     std_autocorr = np.zeros(len(dts))
-    
+
     with ThreadPoolExecutor() as executor:
         results = list(executor.map(compute_autocorr, dts))
-    
+
     for i, (mean, std) in enumerate(results):
         mean_autocorr[i] = mean
         std_autocorr[i] = std
-    
+
     return dts * 0.002, mean_autocorr, std_autocorr
+
 
 x, y, y_std = autocorr_par(normalized_direct_cart)
 
@@ -270,7 +291,11 @@ x, y, y_std = autocorr_par(normalized_direct_cart)
 plt.scatter(x, y, label='Mean Autocorrelation')
 
 # Plot error bars using standard deviation
-plt.errorbar(x, y, yerr=y_std, fmt='o', label='Mean Autocorrelation with Std Dev')
+plt.errorbar(x,
+             y,
+             yerr=y_std,
+             fmt='o',
+             label='Mean Autocorrelation with Std Dev')
 
 plt.xlabel('Time Lag (ps)')
 plt.ylabel('Autocorrelation')
@@ -280,32 +305,36 @@ plt.grid(True)
 plt.show()
 
 #%%
-import numpy as np
-import matplotlib.pyplot as plt
 from concurrent.futures import ThreadPoolExecutor
 
+import matplotlib.pyplot as plt
+import numpy as np
+
+
 def autocorr_par(direct_cart, Npt=100):
+
     def compute_autocorr(dt):
         autocorr = np.sum(direct_cart[:-dt] * direct_cart[dt:], axis=-1)
         mean_autocorr = np.mean(autocorr)
         sem_autocorr = np.std(autocorr) / np.sqrt(len(autocorr))
         return mean_autocorr, sem_autocorr
-    
+
     Nts = len(direct_cart)
     ep = Nts - 50
     dts = np.round(np.logspace(1, np.log10(ep), Npt)).astype(int)
-    
+
     mean_autocorr = np.zeros(len(dts))
     sem_autocorr = np.zeros(len(dts))
-    
+
     with ThreadPoolExecutor() as executor:
         results = list(executor.map(compute_autocorr, dts))
-    
+
     for i, (mean, sem) in enumerate(results):
         mean_autocorr[i] = mean
         sem_autocorr[i] = sem
-    
+
     return dts * 0.002, mean_autocorr, sem_autocorr
+
 
 x, y, y_sem = autocorr_par(normalized_direct_cart, Npt=100)
 
@@ -313,7 +342,11 @@ x, y, y_sem = autocorr_par(normalized_direct_cart, Npt=100)
 #plt.scatter(x, y, label='Mean Autocorrelation')
 
 # Plot error bars using standard error
-plt.errorbar(x, y, yerr=y_sem, fmt='o', label='Mean Autocorrelation with Std Error')
+plt.errorbar(x,
+             y,
+             yerr=y_sem,
+             fmt='o',
+             label='Mean Autocorrelation with Std Error')
 # plt.errorbar(x, y, yerr=y_sem, fmt='s', markersize=0, label='Mean Autocorrelation with Std Error')
 
 plt.xlabel('Time Lag (ps)')
