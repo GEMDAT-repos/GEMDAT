Fix MSD plots units

src/gemdat/plots/matplotlib/_displacements.py
@@ -79,7 +79,7 @@ def msd_per_element(*,
         Input trajectory
     nstarts : int
         Number of starts to use for the MSD calculation. If -1, all starts are
-        used.
+        used throughout the FFT algorithm.
 
     Returns
     -------
@@ -90,15 +90,26 @@ def msd_per_element(*,
 
     fig, ax = plt.subplots()
 
+    # Since we want to plot in picosecond, we convert the time units
+    time_ps = trajectory.time_step * 1e12
+
     for sp in species:
         traj = trajectory.filter(sp.symbol)
-        ax.plot(traj.mean_squared_displacement(nstarts),
-                lw=0.5,
-                label=sp.symbol)
+        msd = traj.mean_squared_displacement(nstarts)
+        msd_mean = np.mean(msd, axis=0)
+        msd_std = np.std(msd, axis=0)
+        t_values = np.arange(len(msd_mean)) * time_ps
+        ax.plot(t_values, msd_mean, lw=0.5, label=sp.symbol)
+        last_color = ax.lines[-1].get_color()
+        ax.fill_between(t_values,
+                        msd_mean - msd_std,
+                        msd_mean + msd_std,
+                        color=last_color,
+                        alpha=0.2)
 
     ax.legend()
     ax.set(title='Mean squared displacement per element',
-           xlabel='Time step',
+           xlabel='Time lag [ps]',
            ylabel='MSD (Angstrom$^2$)')
 
     return fig

src/gemdat/trajectory.py
@@ -496,10 +496,10 @@ class Trajectory(PymatgenTrajectory):
 
         return subtrajectories
 
-    def mean_squared_displacement(self, nstarts: int = 1) -> np.ndarray:
+    def mean_squared_displacement(self, nstarts: int = -1) -> np.ndarray:
         """Compute the mean squared displacement using the specified number of
-        starting points. This number defaults to 1, but can be increased to
-        improve statistics.
+        starting points. This number defaults to -1, corresponding to the FFT
+        method.
 
         Parameters
         ----------
@@ -507,7 +507,7 @@ class Trajectory(PymatgenTrajectory):
             Number of starting points to use for the MSD calculation. If set to -1,
             all possible starting points are used using the FFT method. The algorithm
             also defaults to using the FFT method if the number of starting points is
-            greater than the lenght of the trajectory.
+            greater than the length of the trajectory.
 
         Returns
         -------
@@ -527,7 +527,7 @@ class Trajectory(PymatgenTrajectory):
             # ignoring the nan values created by splitting the trajectory
             msd = np.nanmean(msd, axis=0).transpose(1, 0)
 
-        return msd  #.mean(axis=0)
+        return msd
 
     def _fft_mean_squared_displacement(self, r: np.ndarray) -> np.ndarray:
         """Computes the mean squared displacement using fast Fourier transform.
@@ -544,7 +544,6 @@ class Trajectory(PymatgenTrajectory):
         msd : np.ndarray
             Output array with mean squared displacement per particle
         """
-        print('FFT method')
         pos = np.transpose(r, (1, 0, 2))
         n_times = pos.shape[1]
 