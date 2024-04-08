e5e4250a70768a6bc0384e4566ea2b1880c8f055
SCiarella <58949181+SCiarella@users.noreply.github.com>
2024-04-08 14:30:55 +0200

    Fix Orientations IO (#293)
    * Fix Orientations IO
    
    * Allow different primitive matrices
    
    * Remove duplicate methods
    
    * Update src/gemdat/rotations.py
    
    Co-authored-by: Stef Smeets <stefsmeets@users.noreply.github.com>
    
    * Fix plot test
    
    * Remove .set_transform
    
    * Fix docstrings
    
    * Use functions instead of classes for transforms
    
    * Avoid expensive np sqrt
    
    * Code cleanup
    
    * Return Orientation from transforms
    
    * Refactor Orientations
    
    * Update tests
    
    * Fix line endings
    
    * Update hooks
    
    ---------
    
    Co-authored-by: Stef Smeets <stefsmeets@users.noreply.github.com>
    Co-authored-by: Stef Smeets <s.smeets@esciencecenter.nl>

--- tests/rotations_test.py
+++ tests/rotations_test.py
@@ -5,7 +5,64 @@ from math import isclose
 import numpy as np
 from pymatgen.core import Species
 
-from gemdat.rotations import calculate_spherical_areas, mean_squared_angular_displacement
+from gemdat.rotations import (
+    Orientations,
+    calculate_spherical_areas,
+    mean_squared_angular_displacement,
+)
+
+
+def test_orientations_init(trajectory):
+    orientations = Orientations(trajectory=trajectory,
+                                center_type='B',
+                                satellite_type='Si',
+                                nr_central_atoms=1)
+
+    assert isinstance(orientations, Orientations)
+    assert orientations.center_type == 'B'
+    assert orientations.satellite_type == 'Si'
+    assert orientations.nr_central_atoms == 1
+
+
+def test_normalize(trajectory):
+    orientations = Orientations(trajectory=trajectory,
+                                center_type='B',
+                                satellite_type='Si',
+                                nr_central_atoms=1,
+                                in_vectors=np.array([[1, 2, 2], [2, 2, 1]],
+                                                    dtype=float))
+    ret = orientations.normalize()
+    assert np.allclose(
+        ret.vectors, np.array([[1 / 3, 2 / 3, 2 / 3], [2 / 3, 2 / 3, 1 / 3]]))
+
+
+def test_conventional(trajectory):
+    orientations = Orientations(trajectory=trajectory,
+                                center_type='B',
+                                satellite_type='Si',
+                                nr_central_atoms=1,
+                                in_vectors=np.array(
+                                    [[1, 0, 0], [0, 1, 0], [0, 0, 1]],
+                                    dtype=float))
+    prim_to_conv_matrix = np.eye(3) * [1, 2, 3]
+    ret = orientations.conventional(prim_to_conv_matrix=prim_to_conv_matrix)
+    assert np.allclose(ret.vectors, np.array([[1, 0, 0], [0, 2, 0], [0, 0,
+                                                                     3]]))
+
+
+def test_symmetrize(trajectory):
+    orientations = Orientations(trajectory=trajectory,
+                                center_type='B',
+                                satellite_type='Si',
+                                nr_central_atoms=1,
+                                in_vectors=np.array([[[1, 0, 0]], [[0, 1, 0]]],
+                                                    dtype=float))
+    sym_ops = np.array([[0, -1, 0], [1, 0, 0], [0, 0, -1]])
+    ret = orientations.symmetrize(sym_ops=sym_ops)
+    assert np.allclose(
+        ret.vectors,
+        np.array([[[0., 1., 0.], [-1., 0., 0.], [0., 0., -1.]],
+                  [[0., 1., 0.], [-1., 0., 0.], [0., 0., -1.]]]))
 
 
 def test_orientations(orientations):
