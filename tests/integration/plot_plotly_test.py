694c62d63ba825e4bc9414fcf0e2537f2f68735b
Stef Smeets <s.smeets@esciencecenter.nl>
2024-05-27 09:58:06 +0200

    Set default font and allow small deviation

--- tests/integration/plot_plotly_test.py
+++ tests/integration/plot_plotly_test.py
@@ -17,6 +17,12 @@ def assert_image_similar(fig,
                          name: str,
                          ext: str = 'png',
                          rms: float = 0.0):
+    # Ensure same font is used on different machines (local/CI)
+    fig.update_layout(
+        font_family='Arial',
+        title_font_family='Arial',
+    )
+
     RESULTS_DIR = Path() / 'result_images' / Path(__file__).stem
     RESULTS_DIR.mkdir(exist_ok=True, parents=True)
 
@@ -36,9 +42,9 @@ def assert_image_similar(fig,
 
     expected_link.symlink_to(expected)
 
-    err: dict[str, Any] = compare_images(str(expected_link),
-                                         str(actual),
-                                         0.0,
+    err: dict[str, Any] = compare_images(expected=str(expected_link),
+                                         actual=str(actual),
+                                         tol=rms,
                                          in_decorator=True)  # type: ignore
 
     if err:
@@ -52,59 +58,59 @@ def assert_image_similar(fig,
 def test_displacement_per_element(vasp_traj):
     fig = plots.displacement_per_element(trajectory=vasp_traj)
 
-    assert_image_similar(fig, name='displacement_per_element')
+    assert_image_similar(fig, name='displacement_per_element', rms=0.5)
 
 
 def test_displacement_per_atom(vasp_traj):
     diff_trajectory = vasp_traj.filter('Li')
     fig = plots.displacement_per_atom(trajectory=diff_trajectory)
 
-    assert_image_similar(fig, name='displacement_per_atom')
+    assert_image_similar(fig, name='displacement_per_atom', rms=0.5)
 
 
 def test_displacement_histogram(vasp_traj):
     diff_trajectory = vasp_traj.filter('Li')
     fig = plots.displacement_histogram(trajectory=diff_trajectory)
 
-    assert_image_similar(fig, name='displacement_histogram')
+    assert_image_similar(fig, name='displacement_histogram', rms=0.5)
 
 
 def test_frequency_vs_occurence(vasp_traj):
     diff_traj = vasp_traj.filter('Li')
     fig = plots.frequency_vs_occurence(trajectory=diff_traj)
 
-    assert_image_similar(fig, name='frequency_vs_occurence')
+    assert_image_similar(fig, name='frequency_vs_occurence', rms=0.5)
 
 
 def test_vibrational_amplitudes(vasp_traj):
     diff_traj = vasp_traj.filter('Li')
     fig = plots.vibrational_amplitudes(trajectory=diff_traj)
 
-    assert_image_similar(fig, name='vibrational_amplitudes')
+    assert_image_similar(fig, name='vibrational_amplitudes', rms=0.5)
 
 
 def test_jumps_vs_distance(vasp_jumps):
     fig = plots.jumps_vs_distance(jumps=vasp_jumps)
 
-    assert_image_similar(fig, name='jumps_vs_distance')
+    assert_image_similar(fig, name='jumps_vs_distance', rms=0.5)
 
 
 def test_jumps_vs_time(vasp_jumps):
     fig = plots.jumps_vs_time(jumps=vasp_jumps)
 
-    assert_image_similar(fig, name='jumps_vs_time')
+    assert_image_similar(fig, name='jumps_vs_time', rms=0.5)
 
 
 def test_collective_jumps(vasp_jumps):
     fig = plots.collective_jumps(jumps=vasp_jumps)
 
-    assert_image_similar(fig, name='collective_jumps')
+    assert_image_similar(fig, name='collective_jumps', rms=0.5)
 
 
 def test_jumps_3d(vasp_jumps):
     fig = plots.jumps_3d(jumps=vasp_jumps)
 
-    assert_image_similar(fig, name='jumps_3d')
+    assert_image_similar(fig, name='jumps_3d', rms=0.5)
 
 
 def test_radial_distribution(vasp_rdf_data):
@@ -112,7 +118,7 @@ def test_radial_distribution(vasp_rdf_data):
     for rdfs in vasp_rdf_data.values():
         fig = plots.radial_distribution(rdfs)
 
-    assert_image_similar(fig, name='radial_distribution')
+    assert_image_similar(fig, name='radial_distribution', rms=0.5)
 
 
 @pytest.mark.xfail(reason='not implemented yet')
@@ -121,20 +127,20 @@ def test_shape(vasp_shape_data):
     for i, shape in vasp_shape_data:
         fig = plots.shape(shape)
 
-        assert_image_similar(fig, name='shape_{i}')
+        assert_image_similar(fig, name='shape_{i}', rms=0.5)
 
 
 def test_msd_per_element(vasp_traj):
     fig = plots.msd_per_element(trajectory=vasp_traj[-500:])
 
-    assert_image_similar(fig, name='msd_per_element')
+    assert_image_similar(fig, name='msd_per_element', rms=0.5)
 
 
 def test_energy_along_path(vasp_path):
     structure = load_known_material('argyrodite')
     fig = plots.energy_along_path(path=vasp_path, structure=structure)
 
-    assert_image_similar(fig, name='energy_along_path')
+    assert_image_similar(fig, name='energy_along_path', rms=0.5)
 
 
 def test_rectilinear(vasp_orientations):
@@ -145,17 +151,17 @@ def test_rectilinear(vasp_orientations):
     orientations = vasp_orientations.normalize().transform(matrix=matrix)
     fig = plots.rectilinear(orientations=orientations, normalize_histo=False)
 
-    assert_image_similar(fig, name='rectilinear')
+    assert_image_similar(fig, name='rectilinear', rms=0.5)
 
 
 def test_bond_length_distribution(vasp_orientations):
     fig = plots.bond_length_distribution(orientations=vasp_orientations,
                                          bins=50)
 
-    assert_image_similar(fig, name='bond_length_distribution')
+    assert_image_similar(fig, name='bond_length_distribution', rms=0.5)
 
 
 def test_autocorrelation(vasp_orientations):
     fig = plots.autocorrelation(orientations=vasp_orientations)
 
-    assert_image_similar(fig, name='autocorrelation')
+    assert_image_similar(fig, name='autocorrelation', rms=0.5)
