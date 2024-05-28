06de1583940b86724332ebe9e664c3b0275907ff
Stef Smeets <s.smeets@esciencecenter.nl>
2024-05-28 11:23:32 +0200

    Fix imports

--- src/gemdat/plots/__init__.py
+++ src/gemdat/plots/__init__.py
@@ -3,27 +3,27 @@
 from __future__ import annotations
 
 from .matplotlib import (
-    autocorrelation,
-    bond_length_distribution,
-    energy_along_path,
     jumps_3d_animation,
-    radial_distribution,
-    rectilinear,
     shape,
 )
 
 from .plotly import (
+    autocorrelation,
+    bond_length_distribution,
     collective_jumps,
     density,
     displacement_histogram,
     displacement_per_atom,
     displacement_per_element,
+    energy_along_path,
     frequency_vs_occurence,
     jumps_3d,
     jumps_vs_distance,
     jumps_vs_time,
     msd_per_element,
     plot_3d,
+    radial_distribution,
+    rectilinear,
     vibrational_amplitudes,
 )
 
