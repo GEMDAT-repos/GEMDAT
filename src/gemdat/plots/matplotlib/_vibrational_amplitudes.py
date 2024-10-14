7802e4da12184f69887148c14e1bec817842dedd
Stef Smeets <s.smeets@esciencecenter.nl>
2024-10-14 10:55:50 +0200

    Add boilerplate to matplotlib function [wip]

--- src/gemdat/plots/matplotlib/_vibrational_amplitudes.py
+++ src/gemdat/plots/matplotlib/_vibrational_amplitudes.py
@@ -10,26 +10,56 @@ if TYPE_CHECKING:
     from gemdat.trajectory import Trajectory
 
 
-def vibrational_amplitudes(*, trajectory: Trajectory) -> plt.Figure:
+def vibrational_amplitudes(
+    *, trajectory: Trajectory, bins: int = 50, n_parts: int = 1
+) -> plt.Figure:
     """Plot histogram of vibrational amplitudes with fitted Gaussian.
 
     Parameters
     ----------
     trajectory : Trajectory
         Input trajectory, i.e. for the diffusing atom
+    bins : int
+        Number of bins for the histogram
+    n_parts : int
+        Number of parts for error analysis
 
     Returns
     -------
     fig : matplotlib.figure.Figure
         Output figure
     """
-    metrics = trajectory.metrics()
+    trajectories = trajectory.split(n_parts)
+    single_metrics = trajectory.metrics()
+    metrics = [trajectory.metrics().amplitudes() for trajectory in trajectories]
+
+    max_amp = max(max(metric) for metric in metrics)
+    min_amp = min(min(metric) for metric in metrics)
+
+    max_amp = max(abs(min_amp), max_amp)
+    min_amp = -max_amp
+
+    data = []
+
+    for metric in metrics:
+        data.append(np.histogram(metric, bins=bins, range=(min_amp, max_amp), density=True)[0])
+
+    # offset to middle of bar
+    offset = (max_amp - min_amp) / (bins * 2)
+
+    columns = np.linspace(min_amp + offset, max_amp + offset, bins, endpoint=False)
+
+    mean = [col.mean() for col in columns]
+    std = [col.std() for col in columns]
 
     fig, ax = plt.subplots()
-    ax.hist(metrics.amplitudes(), bins=100, density=True)
+    # hist goes here
+    # ax.hist(metrics.amplitudes(), bins=bins, density=True)
+
+    plt.hist(columns, columns, weights=mean)
 
     x = np.linspace(-2, 2, 100)
-    y_gauss = stats.norm.pdf(x, 0, metrics.vibration_amplitude())
+    y_gauss = stats.norm.pdf(x, 0, single_metrics.vibration_amplitude())
     ax.plot(x, y_gauss, 'r')
 
     ax.set(
