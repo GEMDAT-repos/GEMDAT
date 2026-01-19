903dba45a14295af8c921bf57fead09a1b140d0d
Anastasia K. Lavrinenko <75916717+AstyLavrinenko@users.noreply.github.com>
2026-01-19 06:25:57 +0100

    Fix code style issue

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -143,5 +143,7 @@ def vasp_orientations_spherical(vasp_orientations):
 
 @pytest.fixture()
 def arrhenius_data(trajectory_list):
-    arrhenius = ArrheniusFit.from_trajectories(trajectories=trajectory_list, diffusing_specie='B', n_parts=2)
+    arrhenius = ArrheniusFit.from_trajectories(
+        trajectories=trajectory_list, diffusing_specie='B', n_parts=2,
+    )
     return arrhenius
