190dd32be1c404bfbc4b3210d069aff1593913d4
v1kko <1472570+v1kko@users.noreply.github.com>
2026-07-16 13:17:25 +0000

    Bump version to: 1.7.3

--- src/gemdat/__init__.py
+++ src/gemdat/__init__.py
@@ -11,7 +11,7 @@ from .trajectory import Trajectory
 from .transitions import Transitions
 from .volume import Volume, trajectory_to_volume
 
-__version__ = '1.7.2'
+__version__ = '1.7.3'
 __all__ = [
     'Crystallizer',
     'CrystallizerResult',
